c655f55d5ffbd03b13a68f02d6066535f0f9b1fe
Andy Smith <andy151993@hotmail.com>
2021-09-21 15:17:19 +0100

    add PSQL error handler

--- errors/error-handlers.js
+++ errors/error-handlers.js
@@ -5,6 +5,14 @@ exports.handleServerError = (err, req, res, next) => {
   }
 };
 
+exports.handlePSQLErrors = (err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid type input" });
+  } else {
+    next(err);
+  }
+};
+
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send({ msg: err.msg });
