fc5f471f346351bba776c0ca665cc9fabe712c96
Andy Smith <andy151993@hotmail.com>
2021-09-21 13:01:49 +0100

    add custom error handler

--- errors/error-handlers.js
+++ errors/error-handlers.js
@@ -1,6 +1,12 @@
-exports.handleServerError = (err, res, req, next) => {
+exports.handleServerError = (err, req, res, next) => {
   if (err) {
     console.log(err, "<<<< unhandled error");
     res.status(500).send({ msg: "Internal Server Error" });
+  }
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
   }
 };
