a80aa35fdb5a127fa3fc64fd57fca6735fbb067d
Andy Smith <andy151993@hotmail.com>
2021-09-23 11:41:20 +0100

    prod setup

--- listen.js
+++ listen.js
@@ -1,5 +1,12 @@
 const app = require("./app");
 
-app.listen(9090, () => {
-  console.log("Listening...");
+// app.listen(9090, () => {
+//   console.log("Listening...");
+// });
+
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on ${PORT}...`);
 });
