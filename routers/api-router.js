8379e40e3f7943bb4a0b7c684aa50bca8a329c44
Andy Smith <andy151993@hotmail.com>
2021-09-23 11:05:48 +0100

    added GET /api/ endpoint responding with list of all endpoints

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const articlesRouter = require("./articles-routers");
 const topicsRouter = require("./topics-routers");
+const fs = require("fs/promises");
 
 const apiRouter = express.Router();
+
+//endpoint list
+apiRouter.get("/", async (req, res) => {
+  const endpoints = await fs.readFile("./endpoints.json", "utf-8");
+  res.status(200).send(endpoints);
+});
 
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/topics", topicsRouter);
