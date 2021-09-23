ca0c392d5fc6c4033d1b6cbc01952a2e62a6a8a2
Andy Smith <andy151993@hotmail.com>
2021-09-23 16:00:39 +0100

    add try/catch block to getUsers

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,10 @@
 const { selectUsers } = require("../models/users-models");
 
 exports.getUsers = async (req, res, next) => {
-  const users = await selectUsers();
-  res.status(200).send({ users });
+  try {
+    const users = await selectUsers();
+    res.status(200).send({ users });
+  } catch (err) {
+    next(err);
+  }
 };
