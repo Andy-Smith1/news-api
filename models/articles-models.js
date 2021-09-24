ce80dfd7e9577bbabdcbc3c45a29663d91882777
Andy Smith <andy151993@hotmail.com>
2021-09-24 14:41:51 +0100

    add error handling for non-existent id

--- models/articles-models.js
+++ models/articles-models.js
@@ -38,6 +38,10 @@ exports.updateArticleVotes = async (article_id, body) => {
   SET votes = votes + $1 WHERE article_id = $2 RETURNING*;`,
     [body.inc_votes, article_id]
   );
+
+  if (updatedArticle.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Article not found" });
+  }
   return updatedArticle.rows[0];
 };
 
