9564412aac7cedb4454a14f85471cbfb52d3d057
Andy Smith <andy151993@hotmail.com>
2021-09-24 15:02:52 +0100

    change error code to 404 for nuser not found

--- errors/error-handlers.js
+++ errors/error-handlers.js
@@ -9,7 +9,7 @@ exports.handlePSQLErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Wrong data type" });
   } else if (err.code === "23503") {
-    res.status(400).send({ msg: "User does not exist" });
+    res.status(404).send({ msg: "User does not exist" });
   } else {
     next(err);
   }
