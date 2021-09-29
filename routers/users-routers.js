e597fc7f4f6dc52158fd6a1c86c3986629580cb7
Andy Smith <andy151993@hotmail.com>
2021-09-29 11:23:15 +0100

    add POST method to /api/users

--- routers/users-routers.js
+++ routers/users-routers.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getUsers,
   getUserByUsername,
+  createUser,
 } = require("../controllers/users-controllers");
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", getUsers);
+usersRouter.post("/", createUser);
 usersRouter.get("/:username", getUserByUsername);
 
 module.exports = usersRouter;
