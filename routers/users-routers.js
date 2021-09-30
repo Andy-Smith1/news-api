09d15f96a19806b2b9dcc79395c8a7ed3f5ca59a
Andy Smith <andy151993@hotmail.com>
2021-09-30 12:39:22 +0100

    add PATCH /:username endpoint

--- routers/users-routers.js
+++ routers/users-routers.js
@@ -3,12 +3,17 @@ const {
   getUsers,
   getUserByUsername,
   createUser,
+  editUser,
 } = require("../controllers/users-controllers");
 
 const usersRouter = express.Router();
 
-usersRouter.get("/", getUsers);
-usersRouter.post("/", createUser);
-usersRouter.get("/:username", getUserByUsername);
+usersRouter.route("/").get(getUsers).post(createUser);
+
+// usersRouter.get("/", getUsers);
+// usersRouter.post("/", createUser);
+// usersRouter.get("/:username", getUserByUsername);
+
+usersRouter.route("/:username").get(getUserByUsername).patch(editUser);
 
 module.exports = usersRouter;
