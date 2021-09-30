2e02140335dfc3c797123bb16a3e5aaa6de3ef57
Andy Smith <andy151993@hotmail.com>
2021-09-30 11:54:12 +0100

    add PATCH /comment_id/body endpoint

--- routers/comments-routers.js
+++ routers/comments-routers.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const {
   deleteComment,
-  patchComment,
+  patchCommentVotes,
+  patchCommentBody,
 } = require("../controllers/comments-controllers");
 
 const commentsRouter = express.Router();
 
-commentsRouter.route("/:comment_id").delete(deleteComment).patch(patchComment);
+commentsRouter
+  .route("/:comment_id")
+  .delete(deleteComment)
+  .patch(patchCommentVotes);
+
+commentsRouter.patch("/:comment_id/body", patchCommentBody);
+
 module.exports = commentsRouter;
